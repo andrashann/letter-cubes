4db6781431460237b9125f56b3c1a3e0b21f329a
András Hann <andrashann@users.noreply.github.com>
2021-03-08 01:23:31 +0100

    initial public commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Words in letter blocks
+
+If you have ever found some wooden blocks with letters on them
+and tried to figure out what words you can make using these cubes, look no further
+because this is the project to solve your problems.
+
+Is it unclear what I mean? Just look at this picture:
+
+![a photo of three wooden cubes with different letters on each face](og.jpg)
+
+There is an [API](https://letter-blocks.herokuapp.com/) as well as [a simple front end](http://hann.io/words-in-letter-blocks) (which uses the same API).
+
+Why did I do this? When I visited my mother, we found these old wooden blocks with a letter on each face (see the photo above) and wondered what words we could make using them. Programming is sometimes easier than thinking, so I wrote a script to answer this question.
---
+++ main.py
@@ -0,0 +1,89 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse, JSONResponse
+
+import os
+import codecs
+
+from sympy.utilities.iterables import multiset_permutations
+import jinja2
+
+app = FastAPI(
+    title="Words in letter blocks",
+    description='''If you have a set of cubes with letters on them and are 
+    trying to figure out which words you can make using them, this is the 
+    API you need.
+    
+[https://github.com/andrashann/words-in-letter-blocks](https://github.com/andrashann/words-in-letter-blocks)
+''',
+    version="0.0.1",
+    docs_url="/", redoc_url=None,
+)
+
+
+@app.get("/c/{cubes}", response_class=HTMLResponse)
+def get_caches(cubes: str, dic: str = "en/en_US.dic", json: bool = False):
+    '''Based on a comma separate list of the set of letters on each cube,
+    e.g. 'abcdef,ghijkl,mnopqr' and a 'dic', it returns a list of words
+    that are valid in that language and you could come up with using these
+    cubes. 
+
+    The dic parameter should be the path to a dictionary in the LibreOffice
+    GitHub repository, e.g. "en/en_US.dic" for 
+    https://raw.githubusercontent.com/LibreOffice/dictionaries/master/en/en_US.dic.
+
+    If json=True, it returns a JSON list.'''
+
+    cubes = cubes.split(',')
+
+    if not os.path.exists('dic'):
+        os.makedirs('dic')
+
+    # If we do not have the dictionary file yet, download it
+    filename = dic.split('/')[-1]
+    if not os.path.exists(filename):
+        import urllib.request
+        urllib.request.urlretrieve(f'https://raw.githubusercontent.com/LibreOffice/dictionaries/master/{dic}',
+                                   filename)
+    else:
+        print('Dictonary file already exists.')
+
+    word_list = []
+
+    # The .dic format has extra information about each word at the end,
+    # separated by a symbol, so split the line there.
+    with codecs.open(filename, encoding='utf-8') as f:
+        for word in f.readlines():
+            word_list.append(word.split('/')[0].split('\t')[0].strip())
+
+    results = []
+    for word in word_list:
+        found_match = False
+
+        # don't even look at words that are not the right length
+        if len(word) != len(cubes):
+            continue
+
+        # letters might appear on multiple cubes, so we should check
+        # all possible orders of cubes:
+        for cube_list in multiset_permutations(cubes):
+            if found_match == True:
+                break
+
+            for i, letter in enumerate(word):
+                if letter not in cube_list[i]:
+                    break
+                # if we are at the last letter and everything worked out,
+                # the word is a match!
+                if i == len(word) - 1:
+                    found_match = True
+                    results.append(word)
+
+    if json:
+        return JSONResponse(content=results)
+
+    template = jinja2.Template('''
+    <html><head></head><body><ol>
+    {% for e in results %}<li>{{ e }}</li>{% endfor %}
+    </ol></body></html>''')
+
+    return(template.render(results=results))
